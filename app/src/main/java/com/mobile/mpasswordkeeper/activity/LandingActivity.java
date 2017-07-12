bac1a000f4c6384c29158d60c1d5000c5ccfb210
rashidaattar <rashida050792>
2017-07-12 14:54:59 +0530

    Incremental Update :Using SAF

--- app/src/main/java/com/mobile/mpasswordkeeper/activity/LandingActivity.java
+++ app/src/main/java/com/mobile/mpasswordkeeper/activity/LandingActivity.java
@@ -1,9 +1,11 @@
 package com.mobile.mpasswordkeeper.activity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 import com.mobile.mpasswordkeeper.R;
@@ -41,5 +43,41 @@ public class LandingActivity extends AppCompatActivity {
 
         Intent intent = new Intent(this,OtherListActivity.class);
         startActivity(intent);
+    }
+
+    public void goToImport(View view) {
+
+        performFileSearch();
+    }
+    public void performFileSearch() {
+
+        // ACTION_OPEN_DOCUMENT is the intent to choose a file via the system's file
+        // browser.
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+
+        // Filter to only show results that can be "opened", such as a
+        // file (as opposed to a list of contacts or timezones)
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+        // Filter to show only images, using the image MIME data type.
+        // If one wanted to search for ogg vorbis files, the type would be "audio/ogg".
+        // To search for all documents available via installed storage providers,
+        // it would be "*/*".
+        intent.setType("application/vnd.ms-excel"); //MIME for Excel
+
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK && requestCode == 1){
+            Uri uri = null;
+            if (data != null) {
+                uri = data.getData();
+                Log.i("here", "Uri: " + uri.toString());
+               // showImage(uri);
+            }
+        }
     }
 }
