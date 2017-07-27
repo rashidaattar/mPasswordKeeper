9dc3631804a9a37d35e9f0b240212cda7839558f
rashidaattar <rashida050792>
2017-07-27 18:23:54 +0530

    Incremental Update :Added icons

--- app/src/main/java/com/mobile/mpasswordkeeper/activity/LandingActivity.java
+++ app/src/main/java/com/mobile/mpasswordkeeper/activity/LandingActivity.java
@@ -1,32 +1,13 @@
 package com.mobile.mpasswordkeeper.activity;
 
-import android.Manifest;
+
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 
 import com.mobile.mpasswordkeeper.R;
-
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellType;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.security.Permission;
-import java.util.Iterator;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
